f91bf25d4c70e0165dba8ba1754a3cff68787d37
Rameesahf <mn22rf@leeds.ac.uk>
2024-10-17 20:24:04 +0100

    Exercise 1: Hello World program

---
+++ user/hw.c
@@ -0,0 +1,8 @@
+#include "kernel/types.h"
+#include "user/user.h"
+
+int main(int argc, char *argv[]) {
+    printf("Hello world\n");  // Print "Hello world"
+    exit(0);                  // Terminate the process
+}
+
